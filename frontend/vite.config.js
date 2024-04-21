64a3b6c8dac3d5ee2cb6848628d001546c5ecbe6
tushar <reachtusharwani@gmail.com>
2024-04-21 05:54:25 -0500

    Add vite-plugin-qrcode to package dependencies and configure in vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import { qrcode } from 'vite-plugin-qrcode';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [react(), qrcode()],
+});
